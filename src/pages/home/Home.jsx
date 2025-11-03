d1060585e06e40bb776eaf06bf8070cdd3cf8761
maribel congo <congoceleste1@gmail.com>
2025-11-03 18:07:39 -0300

    correccion de cv

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,31 +1,3 @@
-// import React from "react";
-// import "./home.css";
-// import { Link } from "react-router-dom";
-// import { useTranslation } from "react-i18next";
-// import logo from "../../assets/image/logo1.png";
-
-// export default function Home() {
-//   const { t } = useTranslation();
-
-//   return (
-//     <section className="section home">
-//       <div className="home-image">
-//         <img src={logo} alt="logo" />
-//       </div>
-
-//       <div className="home-text">
-//         <h1 className="title_Name">Maribel Congo</h1>
-//         <p className="text">{t("home.role")}</p>
-//         <p className="subtitle">{t("home.summary")}</p>
-//         <div className="home-buttons">
-//           <Link to="/about" className="btn-primary">
-//             {t("home.button")}
-//           </Link>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
 import React from "react";
 import "./home.css";
 import { Link } from "react-router-dom";
@@ -55,7 +27,7 @@ export default function Home() {
       </section>
 
       {/* Botón de descarga CV */}
-      <a href="/Maribel_Congo_CV.pdf" download className="cv-btn-fixed">
+      <a href="/maribel congo V_A.pdf" download className="cv-btn-fixed">
         📄 Download CV
       </a>
     </>
